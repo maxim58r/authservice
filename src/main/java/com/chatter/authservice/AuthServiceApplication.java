85baff4beeae1bdef09e34fb6aab73ddac712259
Maxim Serov <maxim58r@gmail.com>
2025-04-09 11:10:56 +0300

    add logs redisTemplateAdapter

--- src/main/java/com/chatter/authservice/AuthServiceApplication.java
+++ src/main/java/com/chatter/authservice/AuthServiceApplication.java
@@ -1,5 +1,6 @@
 package com.chatter.authservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 /**
  * Main class for the authentication service.
  */
+@Slf4j
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class AuthServiceApplication {
@@ -16,6 +18,12 @@ public class AuthServiceApplication {
      * @param args the input arguments
      */
     public static void main(String[] args) {
+
+        log.debug("Environment Variables:");
+        log.debug("IFT_REDIS_HOST: {}", System.getenv("IFT_REDIS_HOST"));
+        log.debug("IFT_REDIS_PORT: {}", System.getenv("IFT_REDIS_PORT"));
+        log.debug("IFT_REDIS_PASSWORD: {}", System.getenv("IFT_REDIS_PASSWORD"));
+
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 
--- src/main/java/com/chatter/authservice/user/infrastructure/adapter/RedisTemplateAdapter.java
+++ src/main/java/com/chatter/authservice/user/infrastructure/adapter/RedisTemplateAdapter.java
@@ -1,15 +1,24 @@
 package com.chatter.authservice.user.infrastructure.adapter;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class RedisTemplateAdapter {
     private final RedisTemplate<String, Object> redisTemplate;
+
+    public RedisTemplateAdapter(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+
+        // Логирование инициализации RedisTemplate
+        log.debug("RedisTemplate initialized: {}", redisTemplate != null);
+    }
 
     public void cacheToken(String key, Object value, long timeout, TimeUnit unit) {
         redisTemplate.opsForValue().set(key, value, timeout, unit);
